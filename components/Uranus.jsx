7a35550a77179bc16a26b66239bec1799977229c
cprashanth0 <chinmayprashanth@gmail.com>
2026-03-31 18:58:05 -0400

    Update Uranus.jsx

--- components/Uranus.jsx
+++ components/Uranus.jsx
@@ -1,12 +1,13 @@
 import Orbits from "../Orbits.jsx";
 import { PlanetData } from "../Popup/PlanetData.js";
+import uranusImage from "/images/icons/uranus.png";
 function Uranus({ onPlanetClick, isPaused }) {
   const planetSpeed = isPaused ? 0 : 0.007;
   return (
     <Orbits
       orbitSize={910}
       planetSize={22}
-      image="images/icons/uranus.png"
+      image={uranusImage}
       speed={planetSpeed}
       orbitColor="rgba(150, 235, 255, 0.26)"
       onPlanetClick={() => onPlanetClick(PlanetData.Uranus)}
