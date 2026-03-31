30b2dd193571cea4dd6f49fa3ac02c8f297cb85a
cprashanth0 <chinmayprashanth@gmail.com>
2026-03-31 18:47:39 -0400

    Update Venus.jsx

--- components/Venus.jsx
+++ components/Venus.jsx
@@ -1,4 +1,5 @@
 import Orbits from "../Orbits.jsx";
+import venusImage from "/images/icons/venus.png";
 import { PlanetData } from "../Popup/PlanetData.js";
 
 function Venus({ onPlanetClick, isPaused }){
@@ -7,7 +8,7 @@ function Venus({ onPlanetClick, isPaused }){
     <Orbits
       orbitSize={250}
       planetSize={15}
-      image="/images/icons/venus.png"
+      image={venusImage}
       speed={planetSpeed}
       orbitColor="rgba(230, 190, 120, 0.28)"
       onPlanetClick={() => onPlanetClick(PlanetData.Venus)}
